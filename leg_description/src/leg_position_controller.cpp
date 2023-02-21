8737a5842de4fa4d03e1a80a1248469192a288e6
PapaVeneti <michael.d.papadakis@gmail.com>
2023-02-21 19:23:38 +0200

    TestHLC

--- leg_description/src/leg_position_controller.cpp
+++ leg_description/src/leg_position_controller.cpp
@@ -1,4 +1,6 @@
 #include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <control_msgs/JointControllerState.h>
 
 
 class PositionController {
@@ -26,16 +28,85 @@ PositionController(): q1d(0), q2d(0), q3d(0) {
 
     #pragma endregion 
 
+
+    isValid = true;
     // Set zero initial goal
     xd =0;
     yd =0;
     zd =0;
 
+    //Set up goal server
+    // ros::NodeHandle GoalServerHandle = ros::NodeHandle();
+    // GoalServer = GoalServerHandle.advertiseService("GoalSet",&PositionController::setTarget, this);
+
+    //Set up command publishers
+    ros::NodeHandle n11;
+    Joint1_command = n11.advertise<std_msgs::Float64>("/leg/joint1_position_controller/command", 1000);
+
+    ros::NodeHandle n21;
+    Joint2_command = n21.advertise<std_msgs::Float64>("/leg/joint2_position_controller/command", 1000);
+
+    ros::NodeHandle n31;
+    Joint3_command = n31.advertise<std_msgs::Float64>("/leg/joint3_position_controller/command", 1000);
+
+    ros::NodeHandle n12;
+    Joint1_pos = n12.subscribe("/leg/joint1_position_controller/state",1000,&PositionController::PoseCallback1,this);
+
+    ros::NodeHandle n22;
+    Joint2_pos = n22.subscribe("/leg/joint2_position_controller/state",1000,&PositionController::PoseCallback2,this);
+
+    ros::NodeHandle n32;
+    Joint3_pos = n32.subscribe("/leg/joint2_position_controller/state",1000,&PositionController::PoseCallback3,this);
+
+
 }
 
+bool getValid(){
+  return isValid;
+}
 
+void loop(){
+  
+  //Callbacks (get new destination, get new positions)
+  ros::spinOnce();
+
+    // will be changed
+    q1m.data = 0;
+    q2m.data = 0;
+    q3m.data = 0;
+   
+    ROS_INFO_STREAM("States: q1 = "<< q1 <<", q2 = "<< q2 <<", q3 = "<< q3 <<"\n" );
+    //
+  
+  Joint1_command.publish(q1m);
+  Joint2_command.publish(q2m);
+  Joint3_command.publish(q3m);
+
+}
 
 private:
+
+//Callback function to be changed
+#pragma region
+void PoseCallback1(const control_msgs::JointControllerState::ConstPtr& msg) {
+   q1 = msg->process_value; 
+}
+
+void PoseCallback2(const control_msgs::JointControllerState::ConstPtr& msg){
+   q2 = msg->process_value; 
+}
+
+void PoseCallback3(const control_msgs::JointControllerState::ConstPtr& msg){
+   q3 = msg->process_value; 
+}
+
+// void PoseCallback(const control_msgs::JointControllerState::ConstPtr& msg,double q1){
+//    q1 = msg->process_value; 
+// }
+
+// funCall = std::bind(RobotController::PoseCallback,q1); 
+#pragma endregion
+  
 
  // geometry
   double LB0 = 0.05555;
@@ -45,13 +116,13 @@ private:
   double L3E = 0.2455;
   double H = 0.4;
 
-
   // states
   double q1 ,q2 ,q3;
   double q1d,q2d,q3d;
   double xd, yd, zd;
 
-//   std_msgs::Float64 q1m ,  q2m ;
+  //msgs to publish desired states
+  std_msgs::Float64 q1m,q2m,q3m;
 
   //logic flag
   bool isValid;  
@@ -64,12 +135,27 @@ private:
   ros::Subscriber Joint2_pos;
   ros::Subscriber Joint3_pos;
 
-
-
 };
 
-int main(){
+int main(int argc, char **argv){
+  
+  ros::init(argc, argv, "controller");
 
+  //initialize the controller
+  PositionController PC ; 
+
+  //if we don't get the parameters we exit; 
+  if (! PC.getValid() ) return -1; 
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+    
+    PC.loop();
+    loop_rate.sleep();
+
+  }
   return 0;
 }
 
